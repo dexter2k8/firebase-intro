f9abb572ad9a2c3f4489dfb1946740b4c0b4a3e6
Sidnei <neimestre@yahoo.com.br>
2024-10-23 19:32:20 -0300

    updated post multiple incomes endpoint

--- src/app/api/incomes/post-multiple-incomes/route.ts
+++ src/app/api/incomes/post-multiple-incomes/route.ts
@@ -6,6 +6,8 @@ import { db } from "@/services/firebase";
 import { validateUser } from "@/utils/lib";
 import type { IIncome } from "../get-incomes/types";
 
+const brapi_token = process.env.NEXT_PUBLIC_BRAPI_TOKEN;
+
 export async function POST(req: NextRequest) {
   const body: Partial<IIncome>[] = await req.json();
 
@@ -17,10 +19,16 @@ export async function POST(req: NextRequest) {
     const batch = writeBatch(db);
     const incomesRef = collection(db, "incomes");
 
-    body.forEach((income: Partial<IIncome>) => {
+    for (const income of body) {
+      const fund = income.fund_alias;
+      const response = await fetch(`https://brapi.dev/api/quote/${fund}?token=${brapi_token}`).then(
+        (res) => res.json()
+      );
+      const price = response?.results[0].regularMarketPrice ?? 0;
+
       const docRef = doc(incomesRef);
-      batch.set(docRef, { ...income, user_id: uid });
-    });
+      batch.set(docRef, { ...income, user_id: uid, price });
+    }
 
     await batch.commit();
 
