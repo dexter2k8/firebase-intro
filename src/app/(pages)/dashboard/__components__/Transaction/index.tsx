70fad139d7ab2224c126f98f0b70a901e3b61592
Sidnei <neimestre@yahoo.com.br>
2024-11-02 03:16:40 -0300

    updated transactions card

--- src/app/(pages)/dashboard/__components__/Transaction/index.tsx
+++ src/app/(pages)/dashboard/__components__/Transaction/index.tsx
@@ -1,29 +1,27 @@
 import { Tooltip } from "react-tooltip";
 import { formatCurrency, formatDate } from "@/utils/lib";
 import styles from "./styles.module.scss";
-import type { ITransactions } from "@/app/api/get_transactions/types";
+import type { ITransaction } from "@/app/api/transactions/get-transactions/types";
 
-export default function Transaction(props: ITransactions) {
+export default function Transaction(props: ITransaction) {
   const { transaction, head, content, left, right, alias, tag, stats } = styles;
 
   const type = props.quantity < 0 ? "sell" : "buy";
 
   return (
     <div className={transaction}>
-      <p
-        data-tooltip-id="transaction-tooltip"
-        data-tooltip-content={props.description}
-        className={head}
-      >
-        {props.name}
-      </p>
-      <Tooltip id="transaction-tooltip" style={{ maxWidth: "18rem" }} />
+      <div className={head}>
+        <p data-tooltip-id="transaction-tooltip" data-tooltip-content={props.description}>
+          {props.name}
+        </p>
+        <small>{formatDate(props.bought_at)}</small>
+        <Tooltip id="transaction-tooltip" style={{ maxWidth: "18rem" }} />
+      </div>
 
       <div className={content}>
         <div className={left}>
           <small className={alias}>{props.fund_alias}</small>
           <div className={stats}>
-            <p>{Math.abs(props.quantity)}x</p>
             <small
               className={tag}
               style={{ backgroundColor: type === "buy" ? "var(--green)" : "var(--red)" }}
@@ -33,8 +31,10 @@ export default function Transaction(props: ITransactions) {
           </div>
         </div>
         <div className={right}>
-          <b>{formatCurrency(props.price)}</b>
-          <small>{formatDate(props.bought_at)}</small>
+          <small>
+            {Math.abs(props.quantity)}x {formatCurrency(props.price)}
+          </small>
+          <b>{formatCurrency(props.price * props.quantity)}</b>
         </div>
       </div>
     </div>
