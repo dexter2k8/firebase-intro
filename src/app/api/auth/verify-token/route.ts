9ed01c4187472c7ea2c235e520050263283cccd6
Sidnei <neimestre@yahoo.com.br>
2024-11-10 19:03:19 -0300

    updated verify token

--- src/app/api/auth/verify-token/route.ts
+++ src/app/api/auth/verify-token/route.ts
@@ -1,15 +1,9 @@
 import { onIdTokenChanged } from "firebase/auth";
-import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { auth } from "@/services/firebase";
-import type { JwtPayload } from "jsonwebtoken";
+import { validateUser } from "@/utils/lib";
 import type { NextRequest } from "next/server";
-
-const validKids = [
-  "718f4df92ad175f6a0307ab65d8f67f054fa1e5f",
-  "8d9befd3effcbbc82c83ad0c972c8ea978f6f137",
-];
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
@@ -17,12 +11,9 @@ export async function POST(req: NextRequest) {
   if (!token) return NextResponse.json(false, { status: 200 });
 
   try {
-    const decoded = jwt.decode(token, { complete: true }) as JwtPayload;
-    if (!decoded?.header.kid || !validKids.includes(decoded?.header.kid)) {
-      return NextResponse.json(false, { status: 200 });
-    }
+    if (!validateUser(token)) return NextResponse.json(false, { status: 200 });
 
-    const authenticated = await new Promise<boolean>((resolve) => {
+    const isAuthenticated = await new Promise<boolean>((resolve) => {
       onIdTokenChanged(auth, async (user) => {
         if (user) {
           const newToken = await user.getIdToken();
@@ -40,7 +31,7 @@ export async function POST(req: NextRequest) {
       });
     });
 
-    return NextResponse.json(authenticated, { status: 200 });
+    return NextResponse.json(isAuthenticated, { status: 200 });
   } catch (error) {
     console.error("Erro ao verificar token de App Check:", error);
     return NextResponse.json({ message: "Falha ao verificar o token." }, { status: 500 });
