8e5830951ab1290f205959eb24444f0cced967f7
Sidnei <neimestre@yahoo.com.br>
2024-11-24 16:01:47 -0300

    updated about tab

--- src/app/(pages)/settings/steps/About/index.tsx
+++ src/app/(pages)/settings/steps/About/index.tsx
@@ -6,10 +6,14 @@ export default function About() {
 
   return (
     <>
-      <center>
-        <div className={item}></div>
+      <center className={item}>
         <h3>About</h3>
-        <p>This page is under construction</p>
+        <p>
+          The purpose of this website is to analyze and monitor data on B3 stocks and funds. Data
+          can be entered manually or obtained through Excel reports on the B3 website. If you have
+          any questions or suggestions, please send me an email to neimestre@yahoo.com.br with the
+          subject &quot;Funds Explorer&quot;.
+        </p>
       </center>
     </>
   );
