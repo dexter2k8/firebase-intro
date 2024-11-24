049d4fbe22c42e6fe6c4d8cb7f8cbcc63ab8a42b
Sidnei <neimestre@yahoo.com.br>
2024-11-23 21:00:22 -0300

    updated manage user permission

--- src/app/(pages)/settings/page.tsx
+++ src/app/(pages)/settings/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Tabs from "@/components/Tabs";
+import { useAuth } from "@/store/useAuth";
 import EditProfile from "./steps/EditProfile";
 import { ManageFunds } from "./steps/ManageFunds";
 import { ManageUsers } from "./steps/ManageUsers";
@@ -8,12 +9,13 @@ import type { ITabItemProps } from "@/components/Tabs";
 
 export default function Settings() {
   const { settings } = styles;
+  const { isAdmin } = useAuth();
 
   const tabItems: ITabItemProps[] = [
     { key: 0, label: "Edit profile", children: <EditProfile /> },
     { key: 1, label: "Manage Funds", children: <ManageFunds /> },
   ];
-  if (true) tabItems.push({ key: 2, label: "Manage Users", children: <ManageUsers /> });
+  if (isAdmin) tabItems.push({ key: 2, label: "Manage Users", children: <ManageUsers /> });
 
   return (
     <div className={settings}>
