f68f82a303d52f12262052c39734487c64058750
Sidnei <neimestre@yahoo.com.br>
2026-02-05 11:17:06 -0300

    fix: brapi api replaced with yahoo finance api

--- src/app/api/incomes/post-multiple-incomes/route.ts
+++ src/app/api/incomes/post-multiple-incomes/route.ts
@@ -2,11 +2,12 @@ import { AxiosError } from "axios";
 import { collection, doc, Timestamp, writeBatch } from "firebase/firestore";
 import { cookies } from "next/headers";
 import { type NextRequest, NextResponse } from "next/server";
+import YahooFinance from "yahoo-finance2";
 import { db } from "@/services/firebase";
 import { validateUser } from "@/utils/lib";
 import type { IIncome } from "../get-incomes/types";
 
-const brapi_token = process.env.NEXT_PUBLIC_BRAPI_TOKEN;
+const yahooFinance = new YahooFinance();
 
 export async function POST(req: NextRequest) {
   const body: Partial<IIncome>[] = await req.json();
@@ -14,17 +15,16 @@ export async function POST(req: NextRequest) {
   try {
     const token = cookies().get("funds-explorer-token")?.value;
     const uid = await validateUser(token);
-    if (!uid) return NextResponse.json("Invalid data", { status: 401 });
+    if (!uid) return NextResponse.json("Unauthorized", { status: 401 });
 
     const batch = writeBatch(db);
     const incomesRef = collection(db, "incomes");
 
     for (const income of body) {
       const fund = income.fund_alias;
-      const response = await fetch(`https://brapi.dev/api/quote/${fund}?token=${brapi_token}`).then(
-        (res) => res.json()
-      );
-      const price = response?.results[0].regularMarketPrice ?? 0;
+      const quote = await yahooFinance.quote(`${fund}.SA`);
+      const price = quote?.regularMarketPrice ?? 0;
+
       const updatedAtDate = new Date(income.updated_at!);
       const updated_at = Timestamp.fromDate(updatedAtDate);
 
